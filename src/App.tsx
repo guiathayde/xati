7ccc5af467c55b4f0d86764254183317fa3024f9
Guilherme Athayde <guiathayde303@gmail.com>
2023-03-23 18:31:04 -0300

    Setup lock portrait mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { PromptInstallPWA } from './pages/PromptInstallPWA';
@@ -9,6 +10,13 @@ import { AppProvider } from './hooks';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
+  useEffect(() => {
+    screen.orientation
+      .lock('portrait')
+      .then(() => console.log('Portrait mode locked'))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
     <BrowserRouter>
       <AppProvider>
